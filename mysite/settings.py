32cf15fcf375bd1e7a8d6079d5345af0bf49ba3b
Mariusz Felisiak <felisiak.mariusz@gmail.com>
2026-02-04 10:11:33 +0100

    Revert "Use strict ALLOWED_HOSTS in settings.py example."
    This reverts commit 214ebe2da3662412b1d1a8b6de4d57cd028ed8d1.

--- mysite/settings.py
+++ mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '.aldryn.io']
+ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS=["https://*.aldryn.io"]
 
 
